8648b143faa77d841aff53c161b65e45b8cce778
Maca <maca@brutalrocks.com>
2022-09-28 21:40:10 -0300

    fix: CSS and Js

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,38 @@
-// 1. Selecciona el elemento en el DOM :  let element = document.getElementById("elementID"); / let element = document.querySelector(".my-element");
+// 1. Selecciona el elemento en el DOM :  const element = document.getElementById("elementID"); / const element = document.querySelector(".my-element");
 // 2. Función de controlador de eventos, ej: function showClick()
 // 3. Agregar 1 detector de eventos. element.addEventListener("click", showClick)
 
-//ABRIR la MODAL
-// let open = document.querySelector(".profile__edit-button"); //en botón edit con CSS
-let open = document.getElementById("open"); //seleciono botón edit con ID
-let popupContainer = document.getElementById("popupContainer");
-let show = document.querySelector(".show");
-let close = document.getElementById("close");
+//VARIABLES: TÍTULO / SUBTÍTULO (con ID)
+// const profileTitle = document.querySelector(".profile__title"); //buscar Titulo
+// const profileSubtitle = document.querySelector(".profile__subtitle"); //buscar subtitulo
 
-let profileTitle = document.querySelector(".profile__title"); //buscar Titulo
-let profileSubtitle = document.querySelector(".profile__subtitle"); //buscar subtitulo
+//VARIABLE: ABRIR la MODAL
+const openEditButton = document.getElementById("open-edit-button"); //seleciono botón edit con ID
+//VARIABLES: MODAL / CLASE QUE MUESTRA LA MODAL
+const popupContainer = document.getElementById("popupContainer"); //buscar modal
+const showPopup = document.querySelector(".show"); //variable con css que muestra modal
+//VARIABLE: CERRAR la MODAL
+const closePopup = document.getElementById("close");
 
+//** VARIABLES: MOSTRAR TITULO Y SUBTITULO DEL PERFIL DENTRO DE LOS INPUTS DE LA MODAL:
+const inputTitle = document.getElementById("profileTitle"); // buscar input de título
+const inputSubtitle = document.getElementById("profileSubtitle"); //buscar input de Subtítulo
+const textTitle = document.querySelector(".profile__title"); //buscar texto del título en profile
+const textSubTitle = document.querySelector(".profile__subtitle"); //buscar texto del Subtítulo en profile
+//VARIABLE: MOSTRAR NOMBRE DE INPUTS EN PERFIL
+const formElement = document.getElementById("form"); // buscar el formulario (su ID)
+
+//FUNCIÓN: ABRIR la MODAL
 function showClick() {
   console.log("clic en el elemento");
 
-  popupContainer.className = "show";
+  popupContainer.className = "show"; //cambiar CSS de MODAL con className "show"
   console.log(popupContainer.className); //   MOSTRAR la Modal
 }
 
-open.addEventListener("click", showClick); //detector de evento open.addEventListener()
-close.addEventListener("click", closeClick);
 //** -------
 
-//CERRAR la MODAL
+//FUNCIÓN: CERRAR MODAL
 
 function closeClick() {
   console.log("clic en la X");
@@ -33,12 +42,7 @@ function closeClick() {
 }
 //** -------
 
-//** MOSTRAR TITULO Y SUBTITULO DEL PERFIL DENTRO DE LOS INPUTS DE LA MODAL:
-
-let inputTitle = document.getElementById("profileTitle"); // buscar input de título
-let inputSubtitle = document.getElementById("profileSubtitle"); //buscar input de Subtítulo
-let textTitle = document.querySelector(".profile__title"); //buscar texto del título en profile
-let textSubTitle = document.querySelector(".profile__subtitle"); //buscar texto del Subtítulo en profile
+//** FUNCIÓN: MOSTRAR TITULO Y SUBTITULO DEL PERFIL DENTRO DE LOS INPUTS DE LA MODAL:
 
 function editClick() {
   console.log("clic en botón edit");
@@ -46,12 +50,9 @@ function editClick() {
   inputSubtitle.value = textSubTitle.textContent; //valor del subtítulo es= texto subtítulo
 }
 
-open.addEventListener("click", editClick); //detector de evento open.addEventListener()
 //** -------
 
-//** MOSTRAR NOMBRE DE INPUTS EN PERFIL
-
-let form = document.getElementById("form"); // buscar el formulario (su ID)
+//** FUNCIÓN: MOSTRAR NOMBRE DE INPUTS EN PERFIL
 
 function eventoForm(event) {
   event.preventDefault();
@@ -64,6 +65,16 @@ function eventoForm(event) {
   popupContainer.className = "popup-container"; // Al guardar se CIERRA la modal
 }
 
-form.addEventListener("submit", eventoForm);
-
 //** -------
+
+//EVENTO: ABRIR la MODAL
+openEditButton.addEventListener("click", showClick); //detector de evento open.addEventListener()
+
+//EVENTO: CERRAR la MODAL
+closePopup.addEventListener("click", closeClick);
+
+//** EVENTO: MOSTRAR TITULO Y SUBTITULO DEL PERFIL DENTRO DE LOS INPUTS DE LA MODAL:
+openEditButton.addEventListener("click", editClick); //detector de evento open.addEventListener()
+
+//** EVENTO: MOSTRAR NOMBRE DE INPUTS EN PERFIL
+formElement.addEventListener("submit", eventoForm);
