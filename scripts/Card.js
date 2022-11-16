077d636d34645511e966f728b07df1adcb1698a3
Maca <maca@brutalrocks.com>
2022-11-16 09:33:09 -0300

    Fix: class Card & Utils

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,3 +1,7 @@
+//CARD : crea 1 tarjeta con texto y un enlace de imagen
+
+import { handleOpenPopup, handleClosePopup } from "./utils";
+
 //VARIABLE FORM DE AGREGAR CARDS (a traves de su name)
 const cardForm = document.forms["add-place"]; //Busco el form de imágenes
 
@@ -34,103 +38,107 @@ const imagePopupImage = document.getElementById("addImage");
 //VARIABLE: SELECCIONO TODOS LOS BOTONES DE CERRAR EN EL DOM)
 const closeButtons = document.querySelectorAll(".popup-container__close-popup");
 
+//LIKE BUTTON
+const likeButton = newCard.querySelector(".card__card-like");
+
+//DELETE BUTTON
+const deleteButton = newCard.querySelector(".card__delete-button");
+
 //** -------
 
 export default class Card {
-  constructor(cardSelector, name, link) {
+  constructor(data, cardSelector) {
     this._cardSelector = cardSelector;
-    this._name = name;
-    this._link = link;
+    this._name = data.name;
+    this._link = data.link;
   }
 
-     _getTemplate() {
-      const cardElement = document
-        .querySelector(this._cardSelector)
-        .content.querySelector(".card")
-        .cloneNode(true);
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
 
-      return cardElement;
-    }
+    return cardElement;
+  }
 
-    generateCard() {
-        this._element = this._getTemplate();
-        this._setEventListeners(); // añade los controladores de eventos
-    
-        this._element.querySelector(".card__card-title").textContent = this._name;
-        this._element.querySelector(".card__img-card").style.backgroundImage = `url(${this._link})`;
-    
-        return this._element;
-      }
+  generateCard() {
+    this._element = this._getTemplate();
+    this._setEventListeners(); // añade los controladores de eventos
 
-      items.forEach((item) => {
-        const card = new Card(item, ".card");
-        const cardElement = card.generateCard();
-      
-        // Agrega al DOM
-        document.querySelector(".card-list__items").append(cardElement);
-      });
+    this._element.querySelector(".card__card-title").textContent = this._name;
+    this._element.querySelector(".card__img-card").src = this._link;
+    // this._element.querySelector(
+    //   ".card__img-card"
+    // ).style.backgroundImage = `url(${this._link})`;
 
-    // const newCard = templateCard.cloneNode(true); //clorar elementos de initialCards
-    // const cardImage = newCard.querySelector(".card__img-card"); //elementos de la imagen
-    // cardImage.src = element.link; //link
-    // cardImage.alt = element.name; //alt
-    // newCard.querySelector(".card__card-title").textContent = element.name;
+    return this._element;
+  }
 
-    cardImage.addEventListener("click", (evt) => {
-      imagePopupPicture.src = evt.target.src; //SRC
-      imagePopupPicture.alt = evt.target.alt; // ALT
-      imagePopuptext.textContent = evt.target.alt; // TEXTO
-      openPopup(popupImage);
+  _setEventListeners() {
+    this._element.addEventListener("click", () => {
+      this._handleOpenPopup();
     });
 
-    const likeButton = newCard.querySelector(".card__card-like");
+    closeButtons.addEventListener("click", () => {
+      this._handleClosePopup();
+    });
+  }
 
+  _handleOpenPopup() {
+    handleOpenPopup(this._link);
+  }
+
+  _handleClosePopup() {
+    handleClosePopup(this._link);
+  }
+
+  _likeButton() {
     likeButton.addEventListener("click", () => {
       likeButton.classList.toggle("card__card-like_active"); // escucha al botón LIKE
     });
+  }
 
-    const deleteButton = newCard.querySelector(".card__delete-button");
-
+  _deleteButton() {
     deleteButton.addEventListener("click", () => {
       // escucha al botón ELIMINAR
       deleteButton.closest(".card").remove();
     });
-
-    this._cardContent = newCard;
   }
+}
 
-  //   constructor(cardSelector) {
-  //     this._cardSelector = cardSelector;
-  //   }
+//   constructor(cardSelector) {
+//     this._cardSelector = cardSelector;
+//   }
 
-  //   _getTemplate() {
-  //     const cardElement = document
-  //       .querySelector(this._cardSelector)
-  //       .content.querySelector(".card")
-  //       .cloneNode(true);
+//   _getTemplate() {
+//     const cardElement = document
+//       .querySelector(this._cardSelector)
+//       .content.querySelector(".card")
+//       .cloneNode(true);
 
-  //     return cardElement;
-  //   }
+//     return cardElement;
+//   }
 
-  //   _handleOpenPopup() {
-  //     imagePopupPicture.src = this._link;
-  //     popups.classList.add("popup-container_show");
-  //   }
+//   _handleOpenPopup() {
+//     imagePopupPicture.src = this._link;
+//     popups.classList.add("popup-container_show");
+//   }
 
-  //   _handleClosePopup() {
-  //     imagePopupPicture.src = this._link;
-  //     popups.classList.remove("popup-container_show");
-  //   }
+//   _handleClosePopup() {
+//     imagePopupPicture.src = this._link;
+//     popups.classList.remove("popup-container_show");
+//   }
 
-  //   _setEventListeners() {
-  //     this._element.addEventListener("click", () => {
-  //       this._handleOpenPopup();
-  //     });
+//   _setEventListeners() {
+//     this._element.addEventListener("click", () => {
+//       this._handleOpenPopup();
+//     });
 
-  //     closeButtons.addEventListener("click", () => {
-  //       this._handleClosePopup();
-  //     });
-  //   }
+//     closeButtons.addEventListener("click", () => {
+//       this._handleClosePopup();
+//     });
+//   }
 // }
 
 // export default class DefaultCard extends Card {
@@ -153,8 +161,7 @@ export default class Card {
 //   }
 // }
 
-
-//-- código crear card
+//-- código crear card ANTIGUO
 
 // constructor(element) {
 //     const newCard = templateCard.cloneNode(true); //clorar elementos de initialCards
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,1 +1,22 @@
-//controladores de eventos y la función que abre/cierra las ventanas modales.
+//UTILS: controladores de eventos y la función que abre/cierra las ventanas modales.
+
+//VARIABLE DE TODOS LOS POPUPS
+const popups = Array.from(document.querySelectorAll(".popup-container"));
+
+//VARIABLE MODAL IMAGEN
+const popupImage = document.getElementById("popupImage"); //buscar modal popupImage por ID
+
+//VARIABLE: SELECCIONO TODOS LOS BOTONES DE CERRAR EN EL DOM)
+const closeButtons = document.querySelectorAll(".popup-container__close-popup");
+
+export function handleOpenPopup() {
+  popupImage.src = this._link;
+  popups.classList.add("popup-container_show");
+}
+
+export function handleClosePopup() {
+  popupImage.src = this._link;
+  popups.classList.remove("popup-container_show");
+}
+
+closeButtons.addEventListener("click", handleClosePopup);
